6b6f41a54a8acfc3b2378b455a70e67bb53680c9
Andrea Frittoli <andrea.frittoli@gmail.com>
2018-08-28 22:02:48 -0700

    Up to binary classification intro

--- graphs/all_graphs.sh
+++ graphs/all_graphs.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 
 # Graphs for binary classification
-./metrics_by_feature.sh $@
-./metrics_by_sampling.sh $@
+echo "======> metrics_by_feature" | tee -a all.log
+./metrics_by_feature.sh $@ &>> all.log
+echo "======> metrics_by_sampling" | tee -a all.log
+./metrics_by_sampling.sh $@ &>> all.log
 
 # Graphs for multi-classification - all classes
-CLASS_LABEL=node_provider_all ./metrics_by_feature.sh $@
-CLASS_LABEL=node_provider_all ./metrics_by_sampling.sh $@
-CLASS_LABEL=node_provider_all ./metrics_by_topology.sh $@
+echo "======> metrics_by_feature node all" | tee -a all.log
+CLASS_LABEL=node_provider_all ./metrics_by_feature.sh $@ &>> all.log
+echo "======> metrics_by_sampling node all" | tee -a all.log
+CLASS_LABEL=node_provider_all ./metrics_by_sampling.sh $@ &>> all.log
+echo "======> metrics_by_topology node all" | tee -a all.log
+CLASS_LABEL=node_provider_all ./metrics_by_topology.sh $@ &>> all.log
 
 # Graphs for multi-classification - reduced classes
-CLASS_LABEL=node_provider ./metrics_by_feature.sh $@
-CLASS_LABEL=node_provider ./metrics_by_sampling.sh $@
-CLASS_LABEL=node_provider ./metrics_by_topology.sh $@
+echo "======> metrics_by_feature node" | tee -a all.log
+CLASS_LABEL=node_provider ./metrics_by_feature.sh $@ &>> all.log
+echo "======> metrics_by_sampling node" | tee -a all.log
+CLASS_LABEL=node_provider ./metrics_by_sampling.sh $@ &>> all.log
+echo "======> metrics_by_topology node" | tee -a all.log
+CLASS_LABEL=node_provider ./metrics_by_topology.sh $@ &>> all.log
 
 # Graphs for multi-classification comparison
-./metrics_by_feature_and_label.sh $@
-./metrics_by_sampling_and_label.sh $@
+echo "======> metrics_by_feature_and_label" | tee -a all.log
+./metrics_by_feature_and_label.sh $@ &>> all.log
+echo "======> metrics_by_sampling_and_label" | tee -a all.log
+./metrics_by_sampling_and_label.sh $@ &>> all.log
 
 # Graphs for EPOCHS and other datasets comparison
 # TBD
