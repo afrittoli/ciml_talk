c9d7de5bdb25ab7564fde25d23a56fc988f737b5
Andrea Frittoli <andrea.frittoli@gmail.com>
2018-10-17 16:56:07 +0100

    Add scripts to setup datasets and experiments
    Setup all datasets and all experiments in dedicated scripts that
    are suitable to run in the cloud.
    Next a script to trigger all relevant trainings via FfDL.

---
+++ graphs/setup_datasets.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Setup/refresh all datasets required for various experiments
+# It requires CIML to be installed, and aws profile "ibmcloud"
+# to be defined.
+# It assumes enough examples are cached already
+
+# Call with --force to force recreating datasets
+
+BATCH=128
+EPOCHS=${EPOCHS:-500}
+DATA_PATH=${DATA_PATH:-/git/github.com/mtreinish/ciml/data}
+TARGET_DATA_PATH=${TARGET_DATA_PATH:-/git/github.com/mtreinish/ciml/data}
+SLICE=${SLICE:-":2000"}
+FILENAME_SUFFIX=${FILENAME_SUFFIX:-""}
+S3_AUTH_URL=${S3_AUTH_URL:-https://s3.eu-geo.objectstorage.softlayer.net}
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-}
+
+
+function create_datasets() {
+  for feature_regex in ${FEATURES}; do
+    for sampling in ${SAMPLINGS}; do
+      for class_label in ${CLASS_LABELS}; do
+        for build_name in ${BUILD_NAMES}; do
+        DATASET=$(echo $feature_regex | tr "|" "_" | sed -e "s/(//g" -e "s/)//g")-${sampling}-${class_label}
+        echo "=== Setting up dataset $DATASET"
+          # Build the dataset
+          ciml-build-dataset --dataset $DATASET \
+            --build-name $build_name \
+            --slicer $SLICE \
+            --sample-interval "$sampling" \
+            --features-regex "$FEATURES" \
+            --class-label $class_label \
+            --tdt-split 7 0 3 \
+            --data-path $DATA_PATH \
+            --target-data-path $TARGET_DATA_PATH
+            --s3-url $S3_AUTH_URL $@
+        done
+      done
+    done
+  done
+}
+
+# Dataset by feature/label
+FEATURES="(usr|used|1m) (usr|1m) (usr|used) (usr) (used) (1m)"
+CLASS_LABELS="node_provider_all node_provider"
+SAMPLING=1min
+BUILD_NAMES="tempest-full tempest-full-py3"
+create_datasets()
+
+# Dataset by sampling/label
+FEATURES="(usr|1m)"
+CLASS_LABELS="node_provider_all node_provider"
+SAMPLING="1s 10s 30s 1min 5min 10min"
+BUILD_NAMES="tempest-full tempest-full-py3"
+create_datasets()
---
+++ graphs/setup_experiments.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Setup/refresh all experiments
+# It requires CIML to be installed, and aws profile "ibmcloud"
+# to be defined.
+
+# Call with --force to force recreating datasets
+
+BATCH=128
+TARGET_DATA_PATH=${TARGET_DATA_PATH:-/git/github.com/mtreinish/ciml/data}
+SLICE=${SLICE:-":2500"}
+
+declare -A NETWORK_NAMES
+NETWORK_NAMES["10/10/10"]=dnn-3x10
+NETWORK_NAMES["100/100/100"]=dnn-3x100
+NETWORK_NAMES["100/100/100/100/100"]=dnn-5x100
+NETWORK_NAMES["500/500/500/500/500"]=dnn-5x500
+NETWORK_NAMES["100/100/100/100/100/100/100/100/100/100"]=dnn-10x100
+NETWORK_NAMES["1000/1000/1000"]=dnn-3x1000
+
+EPOCHS="100 500 1000 5000"
+
+for network in "${!NETWORK_NAMES[@]}"; do
+  for epochs in ${EPOCHS}; do
+    # Setup the experiment
+    EXPERIMENT=${network}-${epochs}epochs-bs${BATCH}
+    echo "=== Setting up experiment $EXPERIMENT"
+    ciml-setup-experiment --experiment $EXPERIMENT \
+      --estimator tf.estimator.DNNClassifier \
+      --hidden-layers $network \
+      --steps $(( 2500 / BATCH * EPOCHS )) \
+      --batch-size $BATCH \
+      --epochs ${epochs} \
+      --data-path $TARGET_DATA_PATH $@
+  done
+done
