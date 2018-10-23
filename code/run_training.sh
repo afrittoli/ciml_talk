2d48c9501b215094bab800f5cb2316286061f6cd
Andrea Frittoli <andrea.frittoli@gmail.com>
2018-10-23 08:15:33 +0100

    Updated data on training by sampling for status

--- code/run_training.sh
+++ code/run_training.sh
@@ -1,5 +1,8 @@
-# Train the model based on the dataset and experiment
+# Train the model locally based on the dataset and experiment
 # Store the evaluation metrics as a JSON file
 ciml-train-model --dataset cpu-load-1min-dataset \
   --experiment dnn-5x100 \
   --data-path s3://cimldatasets
+
+# Train the same model in a FfDL cluster
+ffd_train.sh cpu-load-1min-dataset dnn-5x100
